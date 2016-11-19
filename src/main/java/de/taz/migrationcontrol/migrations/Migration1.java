b5d6e34248b7939f1ebdd9a91a0fc65d497d8af7
Robert Schuster <robert.schuster.rs01@gmail.com>
2016-11-19 06:49:10 +0100

    * set up initial plugin infrastructure

---
+++ src/main/java/de/taz/migrationcontrol/migrations/Migration1.java
@@ -0,0 +1,37 @@
+package de.taz.migrationcontrol.migrations;
+
+import static de.taz.migrationcontrol.DTOHelper.NS;
+
+import de.deepamehta.accesscontrol.AccessControlService;
+import de.deepamehta.core.Topic;
+import de.deepamehta.core.service.Inject;
+import de.deepamehta.core.service.Migration;
+import de.deepamehta.core.service.accesscontrol.SharingMode;
+import de.deepamehta.workspaces.WorkspacesService;
+
+/**
+ */
+public class Migration1 extends Migration {
+
+	@Inject
+	private WorkspacesService wsService;
+
+	@Inject
+	private AccessControlService acService;
+
+	/** Creates workspaces for Gamechangers:
+	 * - types: Gamechangers topic types
+	 * - data: Persons, groups, institutions, etc
+	 * - comments: Comments and proposals
+	 */
+	@Override
+	public void run() {
+		Topic typesWs = wsService.createWorkspace("Migration-ControlTypes", NS("workspace.types"),
+				SharingMode.PUBLIC);
+		acService.setWorkspaceOwner(typesWs, AccessControlService.ADMIN_USERNAME);
+
+		Topic dataWs = wsService.createWorkspace("Migration-Control Data", NS("workspace.data"),
+				SharingMode.PUBLIC);
+		acService.setWorkspaceOwner(dataWs, AccessControlService.ADMIN_USERNAME);
+	}
+}

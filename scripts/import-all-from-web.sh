ac1084c07c759780aca73745f1f9b9337d1e86fd
Robert Schuster <robert.schuster.rs01@gmail.com>
2016-12-07 23:09:08 +0100

    * added import script that works with online resources only

---
+++ scripts/import-all-from-web.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+oda="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=1935879601&single=true&output=csv"
+hdi="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=981027257&single=true&output=csv"
+migrationintensity="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=1539190958&single=true&output=csv"
+remittances="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=234680878&single=true&output=csv"
+
+findings="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=192140939&single=true&output=csv"
+factsheet="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=1148106720&single=true&output=csv"
+
+repatration_treaties="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=85677121&single=true&output=csv"
+other_treaties="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=330246598&single=true&output=csv"
+
+theses="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=1729530318&single=true&output=csv"
+background="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=775284486&single=true&output=csv"
+
+imprint="https://docs.google.com/spreadsheets/d/1INPdNSijx20bvTgyTVlnbSVJo782sVhIfcEterd6r7M/pub?gid=966956331&single=true&output=csv"
+
+sessionid=${1}
+scratchdir=`mktemp -d`
+
+echo "Scratch folder" $scratchdir
+echo "Session Id" $sessionid
+importFromWeb() {
+	local type=$1
+	local output=$scratchdir/$type
+	local url=${2}
+
+	echo "Downloading" $type
+	curl -s ${url} --output $output
+	
+	echo "Importing" $type
+	./import.sh $type $output $sessionid
+
+	echo "Done"
+}
+
+importFromWeb oda ${oda}
+importFromWeb hdi ${hdi}
+importFromWeb remittances ${remittances}
+importFromWeb migrationintensity ${migrationintensity}

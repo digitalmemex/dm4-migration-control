c4b9e35ba480a2bf62b66779b01e362caba6f293
Robert Schuster <robert.schuster.rs01@gmail.com>
2016-12-08 02:26:01 +0100

    * enforce UTF-8, just to be sure

--- scripts/import.sh
+++ scripts/import.sh
@@ -4,4 +4,4 @@ type=${1}
 file=${2}
 sessionid=${3}
 
-curl -H 'Content-Type: text/plain' -X PUT --cookie "JSESSIONID=${sessionid}" --data-binary "@${file}" http://localhost:8080/migrationcontrol/v1/import/${type}
+curl -H 'Content-Type: text/plain; charset=UTF-8' -X PUT --cookie "JSESSIONID=${sessionid}" --data-binary "@${file}" http://localhost:8080/migrationcontrol/v1/import/${type}
